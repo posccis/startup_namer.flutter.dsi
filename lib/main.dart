c3ae949d4dedf1def81379ebb9962b58427164df
Posccis <lkzq33@gmail.com>
2022-03-06 11:10:23 -0300

    feat(topico-2): adicionada a opção de favoritar e mudar para cards

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:core';
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
@@ -15,10 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: RandomWords()
-    );
+    return MaterialApp(title: 'Welcome to Flutter', home: RandomWords());
   }
 }
 
@@ -27,21 +26,69 @@ class RandomWords extends StatefulWidget {
   _RandomWordsState createState() => _RandomWordsState();
 }
 
+class AppController extends ChangeNotifier {
+  static AppController instance = AppController();
+
+  bool isSwitched = false;
+  changeView() {
+    isSwitched = !isSwitched;
+    notifyListeners();
+  }
+}
+
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('StartUp Name Generator', style: TextStyle(color: Colors.black),),
-        
-        titleTextStyle: TextStyle(color: Colors.black),
-        backgroundColor: Colors.white,
-      ),
-      
-      body: _buildSuggestions(),
+    return AnimatedBuilder(
+        animation: AppController.instance,
+        builder: (context, child) {
+          return MaterialApp(
+              home: Scaffold(
+                  appBar: AppBar(
+                    title: Text(
+                      'StartUp Name Generator',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    actions: [
+                      Center(
+                        child: Switch(
+                          value: AppController.instance.isSwitched,
+                          onChanged: (value) {
+                            AppController.instance.changeView();
+                          },
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: _pushSaved,
+                        icon: const Icon(Icons.list),
+                        tooltip: 'Saved Suggestions',
+                      )
+                    ],
+                    titleTextStyle: TextStyle(color: Colors.black),
+                    backgroundColor: Colors.white,
+                  ),
+                  body: AppController.instance.isSwitched
+                      ? _buildGridView()
+                      : _buildSuggestions()));
+        });
+  }
+
+  Widget _buildGridView() {
+    return GridView.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 2,
+      crossAxisSpacing: 3,
+      children: List.generate(_suggestions.length, (int i) {
+        final index = i ~/ 2;
+        if (i >= _suggestions.length || _suggestions.length <= 0) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[i]);
+      }),
     );
   }
 
@@ -63,11 +110,49 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      tileColor: Colors.white10,
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from favorites' : 'Save',
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return ListTile(
+            title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ));
+      });
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+          : <Widget>[];
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Saved Suggestions"),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
 }
